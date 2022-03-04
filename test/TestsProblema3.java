e1b2eec0b41f549c5e2882e6f2991628e6b1b48a
Marco San Bartolomé González <marco.sbartolomegonzalez@iesfm.onmicrosoft.com>
2022-03-04 10:03:53 +0100

    Solucion Problema 3

---
+++ test/TestsProblema3.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class TestsProblema2 {
+    @Test
+    public void test_problema_1(){
+        Problema2 problema2 = new Problema2();
+        int numero_inicial = 4;
+        int numero_final = 33;
+        problema2.cadena_de_numeros(numero_inicial, numero_final);
+
+    }
+}
