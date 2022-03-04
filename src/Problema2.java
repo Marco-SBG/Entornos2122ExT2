0768240c9684c6dbdbe318759759c10e13f53e01
Marco San Bartolomé González <marco.sbartolomegonzalez@iesfm.onmicrosoft.com>
2022-03-04 09:34:28 +0100

    Solucion Problema 2

---
+++ src/Problema2.java
@@ -0,0 +1,15 @@
+public class Problema1 {
+    public void cadena_de_numeros(){
+        for (int i = 1; i <= 20; i++) {
+            if (i % 3 == 0){
+                System.out.print("Fizz");
+            }
+            else if (i % 5 == 0){
+                System.out.print("Buzz");
+            }
+            else {
+                System.out.print(i);
+            }
+        }
+    }
+}
---
+++ test/TestsProblema2.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestsProblema1 {
+    @Test
+    public void test_problema_1(){
+        Problema1 problema1 = new Problema1();
+        problema1.cadena_de_numeros();
+    }
+}
